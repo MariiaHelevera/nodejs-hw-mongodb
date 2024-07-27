f83a31fac5d560e87164c4c53591690353d06325
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-07-27 17:51:37 +0200

    added post, delete and patch

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getContactsController, getContactByIdController } from "../controllers/contacts.js";
+import { getContactsController, getContactByIdController, createContactController, deleteContactController, patchContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
@@ -7,5 +7,11 @@ const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -19,7 +19,10 @@ export const setupServer = () => {
         }),
     );
 
-    app.use(express.json());
+    app.use(express.json({
+        type: ['application/json', 'application/vnd.api+json'],
+        limit: '100kb',
+    }));    
     app.use(cors());
 
     app.use(contactRoutes);
