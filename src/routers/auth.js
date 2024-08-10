2d0959d2497d93d292d49937b9a233986f247e4a
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-08-10 02:29:34 +0200

    request reset email added

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,6 +3,8 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
 import { registerUserController, loginUserController, logoutUserController, refreshUserSessionController } from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { requestResetEmailSchema } from '../validation/auth.js';
+import { requestResetEmailController } from '../controllers/auth.js';
 
 const authRouter = Router();
 
@@ -21,5 +23,11 @@ authRouter.post(
 authRouter.post('/logout', ctrlWrapper(logoutUserController));
 
 authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+authRouter.post(
+  '/request-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
 
 export default authRouter;
